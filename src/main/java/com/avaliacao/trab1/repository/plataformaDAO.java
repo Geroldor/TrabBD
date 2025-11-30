b05d6ca10ae714db0ecd206c7c7ae3a4b4b09159
xmalcon <130196933+xmalcon@users.noreply.github.com>
2025-11-30 17:05:13 -0300

    Update plataformaDAO.java

--- src/main/java/com/avaliacao/trab1/repository/plataformaDAO.java
+++ src/main/java/com/avaliacao/trab1/repository/plataformaDAO.java
@@ -1,37 +1,24 @@
 package com.avaliacao.trab1.repository;
 
-import com.avaliacao.trab1.models.plataforma;
+import com.avaliacao.trab1.models.jogogenero;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class plataformaDAO {
+public class jogogeneroDAO {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void salvar(plataforma plataforma) {
-        String sql = "INSERT INTO t1.plataforma (nome_plataforma) VALUES (?)";
-        jdbcTemplate.update(sql, plataforma.getNomePlataforma());
+    public void salvar(jogogenero jogogenero) {
+        String sql = "INSERT INTO t1.jogo_has_genero (id_jogo, id_genero) VALUES (?, ?)";
+        jdbcTemplate.update(sql, jogogenero.getIdJogo(), jogogenero.getIdGenero());
     }
-
-    public List<plataforma> listarTodas() {
-        String sql = "SELECT * FROM t1.plataforma";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(plataforma.class));
-    }
-
-    public plataforma buscarPorId(int id) {
-        String sql = "SELECT * FROM t1.plataforma WHERE id_plataforma = ?";
-        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(plataforma.class), id);
-    }
-
-    public boolean existePorNome(String nome) {
-        String sql = "SELECT COUNT(*) FROM t1.plataforma WHERE LOWER(nome_plataforma) = LOWER(?)";
-        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, nome);
-        return count != null && count > 0;
+    public List<Integer> buscarIdsGenerosPorJogo(int idJogo) {
+        String sql = "SELECT id_genero FROM t1.jogo_has_genero WHERE id_jogo = ?";
+        return jdbcTemplate.queryForList(sql, Integer.class, idJogo);
     }
 
 }
